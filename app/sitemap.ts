054303600f854ebf6e8234771cab9b1fa6fb639b
irfan2904 <shaikirfanahamad2006@gmail.com>
2026-09-17 16:04:17 +0530

    Fix sitemap URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://YOUR-DOMAIN.com",
+      url: "https://saddam-fitness-care.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
